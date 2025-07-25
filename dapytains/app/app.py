2fc1a84ac0c10facd5bca9b2b49146efdc17632d
Thibault Clérice <leponteineptique@gmail.com>
2025-07-25 16:10:29 +0200

    Remove prints

--- dapytains/app/app.py
+++ dapytains/app/app.py
@@ -118,9 +118,7 @@ def document_view(resource, ref, start, end, tree, media, transformer: Transform
         return Response(content, mimetype="application/xml")
 
     doc = Document(collection.filepath)
-    print("Passage !")
     passage = doc.get_passage(ref_or_start=ref or start, end=end, tree=tree)
-    print(media)
     if media != "application/xml":
         return transformer.transform(media, collection, passage)
     else:
